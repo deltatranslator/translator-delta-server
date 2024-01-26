e8b733549789c48884d69d5da23aebbd50357434
Golam Kibria <uchihai850@gmail.com>
2024-01-26 10:27:40 +0600

    Added translation history related apis
    This commit implements all the apis related to translation history functionality of the project.

--- index.js
+++ index.js
@@ -15,36 +15,85 @@ app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri =
-  "mongodb+srv://delta-translator:hzWSRlIt0p80K7sK@cluster0.gspqc3c.mongodb.net/?retryWrites=true&w=majority";
+    "mongodb+srv://delta-translator:hzWSRlIt0p80K7sK@cluster0.gspqc3c.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        const deltaTranslateDB = client.db('deltaTranslateDB');
+        const translationHistoryCollection = deltaTranslateDB.collection('translationHistoryCollection');
+
+        app.get('/translation-history/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { userEmail: email };
+
+            const result = await translationHistoryCollection.findOne(query);
+
+            // const result = userHistory.translationHistory;
+            res.send(result);
+        })
+
+        app.put('/translation-history/:email', async (req, res) => {
+            const email = req.params.email;
+            const updatedTranslationHistory = req.body;
+
+            console.log('hello');
+
+            const filter = { userEmail: email };
+            const existingUser = await translationHistoryCollection.findOne(filter);
+
+            console.log(existingUser);
+
+            if (existingUser) {
+                let tempHistory = [...existingUser.translationHistory];
+                tempHistory.unshift(updatedTranslationHistory.translationHistory[0]);
+
+                if (updatedTranslationHistory.translationHistory.length > 50) {
+                    tempHistory = tempHistory.slice(0, 20);
+                }
+
+                console.log(tempHistory);
+
+                const options = { upsert: true };
+                const updatedDoc = {
+                    $set: {
+                        translationHistory: tempHistory
+                    }
+                }
+                const updateResult = await translationHistoryCollection.updateOne(filter, updatedDoc, options);
+                res.send(updateResult);
+            } else {
+                const insertResult = await translationHistoryCollection.insertOne(updatedTranslationHistory);
+                res.send(insertResult);
+            }
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!"
+        );
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("our delta translator is running fluently");
+    res.send("our delta translator is running fluently");
 });
 app.listen(port, (req, res) => {
-  console.log(`Our translator is running on ${port}`);
+    console.log(`Our translator is running on ${port}`);
 });
